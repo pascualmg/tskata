2e21850cb07d58c310d17c13fc3dd9817422d2c5
passh <pascual.munoz@pccomponentes.com>
2019-10-31 10:35:43 +0100

    files

---
+++ src/holaMundo.ts
@@ -0,0 +1,18 @@
+console.log('hola caracola');
+
+abstract class Human {
+
+
+}
+class Person extends Human{
+}
+
+let Pascual = new Person();
+let cosa = new Person();
+let algo = new Person();
+
+let printName = (name: string) => {
+    console.log('hola');
+}
+
+printName();
