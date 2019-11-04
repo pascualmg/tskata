f02df4c97127ad2b23f3673826b4de101e682572
pascualmg <pascual.munoz@pccomponentes.com>
2019-11-04 23:51:37 +0100

    indent and comment

--- src/Application/getIpUseCase.ts
+++ src/Application/getIpUseCase.ts
@@ -1,13 +1,10 @@
 import {UseCase} from "../Domain/UseCase";
 import {Observable, Observer} from "rxjs";
-import {map, flatMap, pluck} from "rxjs/operators";
+import {map, flatMap, pluck, catchError} from "rxjs/operators";
 import {ajax} from "rxjs/ajax";
 import {XMLHttpRequest} from "xmlhttprequest";
 
-
-
 global.XMLHttpRequest = XMLHttpRequest;
-
 
 export class GetIpUseCase implements UseCase {
     constructor() {
@@ -16,7 +13,8 @@ export class GetIpUseCase implements UseCase {
     handle(getIpHandler: any): any {
         ajax.getJSON('https://api6.ipify.org?format=json').pipe(
             pluck('ip'),
-            flatMap((ip) =>  ajax.getJSON(`http://ip-api.com/json/${ip}`))
+            flatMap((ip) => ajax.getJSON(`http://ip-api.com/json/${ip}`)),
+            //catchError((err) => {console.log(err)})
         ).subscribe({
                 next: (next) => {
                     getIpHandler(next)
