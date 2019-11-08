1b4c8f5e0fae4a951e34e1bb8f653e5ba498f0f8
pascualmg <pascual.munoz@pccomponentes.com>
2019-11-08 13:15:32 +0100

    cleaned and name refactor

--- src/Application/GeoLocateMyIp.ts
+++ src/Application/GeoLocateMyIp.ts
@@ -1,20 +1,30 @@
 import {UseCase} from "../Domain/UseCase";
 import {Observable, Observer} from "rxjs";
-import {map, flatMap, pluck, catchError} from "rxjs/operators";
+import {map, flatMap, pluck, catchError, tap} from "rxjs/operators";
 import {ajax} from "rxjs/ajax";
 import {XMLHttpRequest} from "xmlhttprequest";
 
 global.XMLHttpRequest = XMLHttpRequest;
 
-export class GetIpUseCase implements UseCase {
+//Ejemplo de como con tap podemos hacer colaterales.
+const doSomethingLikeSendEmailOrStoreInRedis = tap(console.log);
+
+/**
+ * Geolocaliza la ciudad de la ip actual de salida al exterior de la máquina
+ * en la que se ejecuta el caso de uso.
+ */
+export class GeoLocateMyIp implements UseCase {
     constructor() {
     }
 
     handle(getIpHandler: any): any {
         ajax.getJSON('https://api6.ipify.org?format=json').pipe(
+            //doSomethingLikeSendEmailOrStoreInRedis,
             pluck('ip'),
             flatMap((ip) => ajax.getJSON(`http://ip-api.com/json/${ip}`)),
-            //catchError((err) => {console.log(err)})
+           // doSomethingLikeSendEmailOrStoreInRedis,
+            pluck('city'),
+            catchError(console.error)
         ).subscribe({
                 next: (next) => {
                     getIpHandler(next)
